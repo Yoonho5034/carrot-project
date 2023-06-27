d8e3cce0c11b4e20e6197d134a50b24e5dac907d
김윤호 <dbsgh5034@naver.com>
2023-06-27 14:33:03 +0900

    에라 모르겠다

--- pages/stream/index.js
+++ pages/stream/index.js
@@ -1,125 +1,31 @@
 const live = () => {
   return (
-    <div className="py-10 px-4  space-y-4">
-      <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-      <div className="mt-5">
-        <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-        <span className="text-2xl block mt-3 text-gray-900">$140</span>
-        <p className=" my-6 text-gray-700">
-          My money&apos;s in that office, right? If she start giving me some
-          bullshit about it ain&apos;t there, and we got to go someplace else
-          and get it, I&apos;m gonna shoot you in the head then and there. Then
-          I&apos;m gonna shoot that bitch in the kneecaps, find out where my
-          goddamn money is. She gonna tell me too. Hey, look at me when I&apos;m
-          talking to you, motherfucker. You listen: we go in there, and that
-          ni**a Winston or anybody else is in there, you the first motherfucker
-          to get shot. You understand?
-        </p>
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
-        <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
+    <div className="py-10 space-y-6 divide-y-2">
+      {[1, 2, 3, 4, 5, 6].map((_, i) => (
+        <div key={i} className="pt-4 px-4 ">
+          <div className="w-full rounded-md bg-slate-300 aspect-video shadow-md" />
+          <h3 className="font-medium text-gray-500 mt-2">
+            Let&apos;s try Potao
+          </h3>
         </div>
-        <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
-          <div className="flex relative max-w-md items-center  w-full mx-auto">
-            <input
-              type="text"
-              className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
-            />
-            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
-                &rarr;
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
+      <button className="fixed bottom-20 right-6 bg-orange-400 rounded-full text-white p-2 hover:bg-orange-500 focus:bg-orange-400 transitions">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+          />
+        </svg>
+      </button>
     </div>
-  )
+  );
 };
 
 export default live;
