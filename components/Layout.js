082bdb475ae44cc312ad4f07bc3856a2ea214985
김윤호 <dbsgh5034@naver.com>
2023-06-28 10:48:37 +0900

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,142 @@
-const Layout = ({ title, canGoBack, hasTabBar, children }) => {
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NAVINFO = [
+  {
+    title: "홈",
+    ref: "/",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "동네생활",
+    ref: "/community",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWdth="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "채팅",
+    ref: "/chats",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "라이브",
+    ref: "/stream",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "나의 당근",
+    ref: "/profile",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+        />
+      </svg>
+    ),
+  },
+];
+
+const Layout = ({ title, canGoBack, hasTabBar = true, children }) => {
+  const router = useRouter();
+  const goToBack = () => {
+    router.back();
+  };
   return (
     <div>
-      <div className="bg-white flex items-center justify-center py-2 fixed w-full max-w-lg border-b-2 font-semibold text-gray-600">
+      <div
+        className={`bg-white flex items-center  py-2 fixed w-full max-w-lg border-b-2 font-semibold text-gray-600  ${
+          canGoBack ? "justify-start" : "justify-center"
+        }`}
+      >
+        {canGoBack && (
+          <button className="ml-4" onClick={goToBack}>
+            &larr;
+          </button>
+        )}
         {title && <span>{title}</span>}
       </div>
-      <div className={hasTabBar ? "py-16" : "pt-16"}>{children}</div>
-      {hasTabBar && <nav></nav>}
+      <div className={hasTabBar ? "pt-16 pb-24" : "py-16"}>{children}</div>
+      {hasTabBar && (
+        <nav className="bg-white text-gray-800 border-t pb-2 fixed bottom-0  pt-3 flex justify-between items-center w-full max-w-lg px-4">
+          {NAVINFO.map(({ title, svg, ref }, i) => {
+            return (
+              <Link href={ref} key={i}>
+                <div className="flex flex-col items-center space-y-2 cursor-pointer">
+                  {svg}
+                  <span>{title}</span>
+                </div>
+              </Link>
+            );
+          })}
+        </nav>
+      )}
     </div>
   );
 };
