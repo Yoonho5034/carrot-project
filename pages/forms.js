97fe232e22ae83b0b87ab14c4c5585b52f238bf1
김윤호 <dbsgh5034@naver.com>
2023-06-29 17:50:39 +0900

    fix: react-hook-form

--- pages/forms.js
+++ pages/forms.js
@@ -7,24 +7,30 @@ const forms = () => {
   const onVaild = () => {
     console.log("first");
   };
+  const onInvalid = (errors) => {
+    console.log(errors);
+  };
   // register : state 연결 (key ,value, onchange, ref)
   // ...register([key],{})
   // watch 로 보면 register key값으로 등록
   return (
-    <form onSubmit={handleSubmit(onVaild)}>
+    <form onSubmit={handleSubmit(onVaild, onInvalid)}>
       <input
-        {...register("userName", { required: true })}
+        {...register("userName", {
+          required: "user name is requ",
+          minLength: { message: "more", value: 5 },
+        })}
         type="text"
         placeholder="UserName"
       />
       <input
         type="email"
-        {...register("email", { required: true })}
+        {...register("email", { required: "email is required" })}
         placeholder="Email"
       />
       <input
         type="password"
-        {...register("password", { required: true })}
+        {...register("password", { required: "password is required" })}
         placeholder="Password"
       />
 
